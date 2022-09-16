39abfe294abea5b42f1d86d7b659834e25ecafab
Konstantin Velly <konstantinvelly@gmail.com>
2022-09-16 12:40:08 +0300

    Changes in the test for draft 3

--- src/test/java/ru/netology/service/CreditCardAppTest.java
+++ src/test/java/ru/netology/service/CreditCardAppTest.java
@@ -1,5 +1,6 @@
 package ru.netology.service;
 
+import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
@@ -16,13 +17,29 @@ class CreditCardAppTest {
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
+
+
+    @Test
+    void ifEverythingIsCorrect() {
+        Configuration.holdBrowserOpen = true;
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Бендер-Задунайский Остап");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79313363998");
+        //driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        //      driver = new ChromeDriver();
+
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -31,15 +48,4 @@ class CreditCardAppTest {
         driver = null;
     }
 
-    @Test
-    void test() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Остап Бендер-Задунайский");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79313363998");
-        driver.findElement(By.className("checkbox")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-    }
-    
 }
